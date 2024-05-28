f02532fb456aacd1e85ba7ecff1d825959663513
Eduar Apaza <eapaza@learnsoft.com>
2024-05-27 22:00:17 -0500

    Using Components, props, states, and useEffect

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const PRODUCTS = [
+  {
+    name: 'Air Jordan 1',
+    url: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8021ed7c-e4fc-497d-81ee-a41047e42465/air-max-plus-drift-mens-shoes-FKwJ7w.png',
+  },
+  {
+    name: 'Air Jordan 1',
+    url: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8021ed7c-e4fc-497d-81ee-a41047e42465/air-max-plus-drift-mens-shoes-FKwJ7w.png',
+  },
+  {
+    name: 'Air Jordan 1',
+    url: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8021ed7c-e4fc-497d-81ee-a41047e42465/air-max-plus-drift-mens-shoes-FKwJ7w.png',
+  },
+  {
+    name: 'Air Jordan 1',
+    url: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8021ed7c-e4fc-497d-81ee-a41047e42465/air-max-plus-drift-mens-shoes-FKwJ7w.png',
+  },
+]
